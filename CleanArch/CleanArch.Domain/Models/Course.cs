e7fc7a5a1e16bc16d638e462c614a55a00ee1952
Araz Pashazadeh Goroly <arazPashazadeh@outlook.com>
2019-11-23 17:51:50 +0330

    Create Course Model

---
+++ CleanArch/CleanArch.Domain/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CleanArch.Domain.Models
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
