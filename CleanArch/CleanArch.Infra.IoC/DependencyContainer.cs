6d993f440f7e4bc9ca0fba04b3c395e4caf00356
Araz Pashazadeh Goroly <arazPashazadeh@outlook.com>
2019-11-24 13:05:41 +0330

    Impeliment IoC

---
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service)
+        {
+            //Application Layer
+            service.AddScoped<ICourseService, CourseService>();
+
+            //Infra Data Layer
+            service.AddScoped<ICourseRepository, CourseRepository>();
+        }
+    }
+}
